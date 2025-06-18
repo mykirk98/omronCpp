10fe5192f1621318c20a73e168314e0d46fbef8e
mykirk98 <mykirk98@gmail.com>
2025-06-18 17:26:21 +0900

    made right annotation

--- myOmronC++/CameraWorker.h
+++ myOmronC++/CameraWorker.h
@@ -5,24 +5,40 @@
 #include <ShlObj.h>
 
 using namespace StApi;
-
+/* 기본적인 카메라 작업을 수행하는 클래스입니다.
+단순한 이미지 획득 및 저장 기능을 제공하며,
+카메라 초기화, 이미지 획득 시작 및 종료 기능을 포함합니다.
+*/
 class CameraWorker
 {
 public:
+	/*
+	클래스 생성자
+	@param imageCount : 수집할 이미지 수
+	*/
 	explicit CameraWorker(uint64_t imageCount = 100);		// 생성자
+	/* 클래스 소멸자 */
 	~CameraWorker();	// 소멸자
-
-	bool initialize();	// 카메라 초기화 및 장치 연결
-	void StartAcquisition();	// 이미지 획득 시작
-	void StopAcquisition();	// 이미지 획득 중지
+	
+	/* 카메라 제어에 필요한 여러 객체 초기화 함수 */
+	bool initialize();
+	/* 이미지 획득 시작 함수 */
+	void StartAcquisition();
+	/* 이미지 획득 종료 함수 */
+	void StopAcquisition();
 	
 protected:
 
 private:
-	GenICam::gcstring SetSavePath();	// 이미지 저장 경로 설정
+	/* 이미지 저장 경로 설정 함수 */
+	GenICam::gcstring SetSavePath();
+	/* StApiRaw 이미지 저장 함수 */
 	bool SaveImage(IStImage* pImage);
+	/* 이미지 로드 함수 */
 	bool LoadImage();
+	/* 이미지 포맷 설정 함수 */
 	bool SetImageFormat();
+	/* BMP 이미지 저장 함수 */
 	bool SaveBMPImage();
 
 	bool m_initialized;
