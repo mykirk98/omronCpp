a831a190e30444297d8c3655992706217c366de6
mykirk98 <mykirk98@gmail.com>
2025-07-02 19:13:53 +0900

    Create a thread pool class that only handles image saving functions.

---
+++ myOmronC++/ImageSaverThreadPool.cpp
@@ -0,0 +1,126 @@
+#include "ImageSaverThreadPool.h"
+//#include <filesystem>
+//#include <iomanip>
+//#include <sstream>
+//#include <iostream>
+
+ImageSaverThreadPool::ImageSaverThreadPool(size_t threadCount, const std::string& saveRootDir, bool convertToColor)
+	: m_running(false)
+	, m_saveRootDir(saveRootDir)
+	, m_convertToColor(convertToColor)
+{
+	// Reserve space for the specified number of threads to avoid frequent reallocations
+	m_workers.reserve(threadCount);
+}
+
+ImageSaverThreadPool::~ImageSaverThreadPool()
+{
+	// Ensure all threads are stopped before destruction
+	Stop();
+}
+
+void ImageSaverThreadPool::Start()
+{
+	m_running = true;
+	
+	for (size_t i = 0; i < m_workers.capacity(); i++)
+	{
+		// Create a new thread and add it to the worker pool
+		m_workers.emplace_back(&ImageSaverThreadPool::WorkerLoop, this);
+		// & : pointer to the WorkerLoop member function
+		// this : pointer to the current instance of ImageSaverThreadPool
+	}
+}
+
+void ImageSaverThreadPool::Stop()
+{
+	m_running = false;
+	// clear the queue to stop processing frames
+	// This will ensure that all threads exit gracefully
+	m_queue.Clear();
+
+	// iterate through the worker threads and join them
+	for (std::vector<std::thread>::iterator it = m_workers.begin(); it != m_workers.end(); ++it)
+	{
+		// Check if the thread is joinable before joining
+		if (it->joinable())
+		{
+			it->join();
+		}
+	}
+	// clear the worker threads vector
+	m_workers.clear();
+}
+
+void ImageSaverThreadPool::Enqueue(const FrameData& frame)
+{
+	// Producer: push a frame into the queue
+	m_queue.Push(frame);
+}
+
+void ImageSaverThreadPool::WorkerLoop()
+{
+	while (m_running)
+	{
+		FrameData frame;
+		if (m_queue.PopWithTimeOut(frame, std::chrono::milliseconds(200)))
+		{
+			try
+			{
+				CIStImageBufferPtr pImageBuffer(CreateIStImageBuffer());
+				ConvertPixelFormat(frame.pImage, m_convertToColor, pImageBuffer);
+
+				GenICam::gcstring savePath = SetSavePath(m_saveRootDir, frame.serialNumber, frame.frameID);
+				SaveImage<BMP>(pImageBuffer, savePath);
+				std::cout << "[ImageSaverThreadPool] Saved image with ID: " << frame.frameID 
+					<< " to path: " << savePath.c_str() << std::endl;
+			}
+			catch (const GenICam::GenericException& e)
+			{
+				std::cerr << "[ImageSaverThreadPool] Error processing frame with ID: " << frame.frameID << e.GetDescription() << std::endl;
+			}
+		}
+	}
+}
+
+void ImageSaverThreadPool::ConvertPixelFormat(IStImage* pSrcImage, bool isColor, CIStImageBufferPtr& pDstBuffer)
+{
+	try
+	{
+		// Create a data converter object for pixel format conversion.
+		CIStPixelFormatConverterPtr pPixelFormatConverter(CreateIStConverter(StConverterType_PixelFormat));
+		
+		if (isColor)
+		{
+			pPixelFormatConverter->SetDestinationPixelFormat(StPFNC_BGR8);
+		}
+		else
+		{
+			pPixelFormatConverter->SetDestinationPixelFormat(StPFNC_Mono8);
+		}
+		// Convert the pixel format of the source image to the destination buffer.
+		pPixelFormatConverter->Convert(pSrcImage, pDstBuffer);
+	}
+	catch (const GenICam::GenericException& e)
+	{
+		std::cerr << "[ImageSaverThreadPool] Converting pixel format error: " << e.GetDescription() << std::endl;
+	}
+}
+
+GenICam::gcstring ImageSaverThreadPool::SetSavePath(const std::string& baseDir, const std::string& cameraName, const uint64_t frameID)
+{
+	try
+	{
+		// Change frameID to string
+		std::string strFrameID = std::to_string(frameID);
+		
+		std::string filePath = baseDir + "\\" + cameraName + "\\" + strFrameID;
+
+		return GenICam::gcstring(filePath.c_str());
+	}
+	catch (const GenICam::GenericException& e)
+	{
+		std::cerr << "[ImageSaverThreadPool] Setting save path error: " << e.GetDescription() << std::endl;
+	}
+	return GenICam::gcstring();
+}
---
+++ myOmronC++/ImageSaverThreadPool.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include "ImageSaveQueue.h"
+#include <thread>
+#include <vector>
+#include <atomic>
+#include <string>
+#include <StApi_TL.h>
+#include <StApi_IP.h>
+
+using namespace StApi;
+
+struct StApiRaw
+{
+	static constexpr const char* extension = ".StApiRaw";
+	static constexpr EStStillImageFileFormat_t fileFormat = StStillImageFileFormat_StApiRaw;
+};
+struct BMP
+{
+	static constexpr const char* extension = ".bmp";
+	static constexpr EStStillImageFileFormat_t fileFormat = StStillImageFileFormat_Bitmap;
+};
+struct TIFF
+{
+	static constexpr const char* extension = ".tif";
+	static constexpr EStStillImageFileFormat_t fileFormat = StStillImageFileFormat_TIFF;
+};
+struct PNG
+{
+	static constexpr const char* extension = ".png";
+	static constexpr EStStillImageFileFormat_t fileFormat = StStillImageFileFormat_PNG;
+};
+struct JPEG
+{
+	static constexpr const char* extension = ".jpg";
+	static constexpr EStStillImageFileFormat_t fileFormat = StStillImageFileFormat_JPEG;
+};
+struct CSV
+{
+	static constexpr const char* extension = ".csv";
+	static constexpr EStStillImageFileFormat_t fileFormat = StStillImageFileFormat_CSV;
+};
+
+/*
+@brief ImageSaverThreadPool class
+@brief This class manages a pool of threads for saving images.
+*/
+class ImageSaverThreadPool
+{
+public:
+	/*
+	@brief ImageSaverThreadPool constructor
+	@param threadCount : Number of threads in the pool
+	@brief saveRootDir : Root directory where images will be saved
+	@brief convertToColor : Flag to indicate whether to convert images to color format
+	*/
+	ImageSaverThreadPool(size_t threadCount, const std::string& saveRootDir, bool convertToColor = false);
+	/* @brief ImageSaverThreadPool destructor */
+	~ImageSaverThreadPool();
+
+	/* @brief Start the thread pool */
+	void Start();
+	/* @brief Stop the thread pool */
+	void Stop();
+	/*
+	@brief Enqueue an image frame for saving
+	@param frame : FrameData object containing image data and metadata
+	*/
+	void Enqueue(const FrameData& frame);
+
+private:
+	/* @brief WorkerLoop function */
+	void WorkerLoop();
+	/*
+	@brief Convert pixel format of the image to the buffer
+	@param pSrcImage : Source image pointer to be converted
+	@param isColor : Flag that decides whether the image is saved in color or mono format
+	*/
+	void ConvertPixelFormat(IStImage* pSrcImage, bool isColor, CIStImageBufferPtr& pDstBuffer);
+	/*
+	@brief Set the save path for the image
+	@param baseDir : Base directory where images will be saved
+	@param cameraName : Name of the camera
+	@param frameID : Frame ID of the image to be saved
+	*/
+	GenICam::gcstring SetSavePath(const std::string& baseDir, const std::string& cameraName, const uint64_t frameID);
+	template<typename FORMAT>
+	void SaveImage(CIStImageBufferPtr& pImageBuffer, GenICam::gcstring& dstDir)
+	{
+		try
+		{
+			dstDir.append(FORMAT::extension);
+
+			CIStStillImageFilerPtr pStillImageFiler(CreateIStFiler(StFilerType_StillImage));
+			pStillImageFiler->Save(pImageBuffer->GetIStImage(), FORMAT::fileFormat, dstDir);
+		}
+		catch (const GenICam::GenericException& e)
+		{
+			std::cerr << "[ImageSaverThreadPool] Saving image error: " << e.GetDescription() << std::endl;
+		}
+	}
+
+	/* @brief Thread pool for saving images */
+	std::vector<std::thread> m_workers;
+	/* @brief ImageSaveQueue object for managing image frames */
+	ImageSaveQueue m_queue;\
+	/* @brief Flag to indicate whether the thread pool is running */
+	std::atomic<bool> m_running;
+	/* @brief Root directory where images will be saved */
+	std::string m_saveRootDir;
+	/* @brief Flag to indicate whether to convert images to color format */
+	bool m_convertToColor;
+};
