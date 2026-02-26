c8bc86cdd49a6bf76ed86c6980aa2129a342a5e4
mykirk98 <mykirk98@gmail.com>
2026-02-26 21:26:46 +0900

    MainWindow.xaml.cs에서 서비스와 ViewModel을 연결하여 UI와 로직을 분리

--- VisionApp/MainWindow.xaml.cs
+++ VisionApp/MainWindow.xaml.cs
@@ -1,24 +1,17 @@
-癤퓎sing System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VisionApp.Services;
+using VisionApp.ViewModels;
 
-namespace VisionApp
+namespace VisionApp;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        var logService = new LogService();  // 로그 서비스 인스턴스 생성
+        var cameraService = new MockCameraService();    // 카메라 서비스 인스턴스 생성 (실제 구현에서는 실제 서비스로 교체)
+        DataContext = new MainViewModel(cameraService, logService); // 뷰모델에 서비스 주입
     }
 }
